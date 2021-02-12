6d88431fe68e50ad7afeb2d2e4ab69df3ba695ed
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-02-12 09:20:45 -0500

    Export working

--- src/Scripts/getPipeData.js
+++ src/Scripts/getPipeData.js
@@ -7,19 +7,53 @@ export default async function getPipeData(exempt) {
 			console.log(`export exempt? ${exempt}`);
 			let loanData = await getAllLoans();
 			let stringData = '';
-			loanData.forEach((row) => {
-                if (!exempt) {
-                    // prettier-ignore
-				stringData = `${stringData}
+			if (!exempt) {
+				loanData.forEach((row) => {
+					// prettier-ignore
+					stringData = `${stringData}
 2|${row.join('|')}`;
-                } else {
-                    // inject exemptions
-                    WORK ON THIS
-                }
-				
-			});
-			
-			//Now transform returned values from [["1","2,"]] to "1|2" newline
+				});
+			} else {
+				loanData.forEach((row) => {
+					row[11] = 'Exempt'; //Street Address
+					row[12] = 'Exempt'; //City
+					row[14] = 'Exempt'; //Zip Code
+					row[57] = 'Exempt'; //Rate Spread
+					row[60] = '1111'; //Credit Score
+					row[61] = '1111'; //CoBorrower Credit Score
+					row[66] = '1111'; //Reason for Denial
+					row[71] = 'Exempt'; //Total Loan Costs
+					row[72] = 'Exempt'; //Total Points and Fees
+					row[73] = 'Exempt'; //Origination Charges
+					row[74] = 'Exempt'; //Discount Points
+					row[75] = 'Exempt'; //Lender Credits
+					row[76] = 'Exempt'; //Interest Rate
+					row[77] = 'Exempt'; //Prepayment Penalty Term
+					row[78] = 'Exempt'; //DTI
+					row[79] = 'Exempt'; //CLTV
+					row[80] = 'Exempt'; //Loan Term
+					row[81] = 'Exempt'; //Intro Rate Period
+					row[82] = '1111'; //Balloon Payment
+					row[83] = '1111'; //Interest Only Payments
+					row[84] = '1111'; //Negative Am
+					row[85] = '1111'; //Other Non-Am
+					row[86] = 'Exempt'; //Property Value
+					row[87] = '1111'; //Manufactured Home Secured Property Type
+					row[88] = '1111'; //Manufactured Home Land Property Interest
+					row[90] = 'Exempt'; //Multifamily # Units
+					row[91] = '1111'; //Submission of Application
+					row[92] = '1111'; //Initially Payable
+					row[93] = 'Exempt'; //NMLSR
+					row[94] = '1111'; //AUS System
+					row[100] = '1111'; //AUS Result
+					row[106] = '1111'; //HECM
+					row[107] = '1111'; //HELOC
+					row[108] = '1111'; //Business or Commercial Purpose
+					// prettier-ignore
+					stringData = `${stringData}
+2|${row.join('|')}`;
+				});
+			}
 			resolve(stringData);
 		} catch (error) {
 			console.log('Error getting org pipe data');
