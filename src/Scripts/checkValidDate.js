db6424ec0c9cd0dc63e39fd6cc8e6cb2ec9cafba
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-03-25 09:12:46 -0400

    Validation
    Run in browser only

--- src/Scripts/checkValidDate.js
+++ src/Scripts/checkValidDate.js
@@ -1,8 +1,10 @@
 export default async function checkValidDate(dt) {
-	return new Promise((resolve, reject) => {
-		let regex = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
-		let result = regex.test(dt);
-		console.log(`validation result for ${dt}: ${regex.test(dt)}`);
-		resolve(regex.test(dt));
-	});
+	if (process.browser) {
+		return new Promise((resolve, reject) => {
+			let regex = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+			let result = regex.test(dt);
+			console.log(`validation result for ${dt}: ${regex.test(dt)}`);
+			resolve(regex.test(dt));
+		});
+	}
 }
