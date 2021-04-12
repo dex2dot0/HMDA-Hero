25482aa9ecc87ab1adb2d0e1f7b212853a5c80bd
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-04-12 09:30:37 -0400

    More validation

--- src/components/Applicant GMI/EthFree.svelte
+++ src/components/Applicant GMI/EthFree.svelte
@@ -1,12 +1,19 @@
 <script>
     import {
-      applicantEthOther
+      applicantEthOther,
+      applicantEth1, 
+      applicantEth2,
+      applicantEth3,
+      applicantEth4,
+      applicantEth5,
     } from "./../../stores.js";
     import Modal from "../Modal.svelte";
+    import HRValidation from "../HRValidation.svelte";
 </script>
 
-<label for="EthFree" style="font-size: 9px;">
-    Free Form for Other Hispanic or Latino
+
+  <label for="EthFree" style="font-size: 12px;">
+    Other Hispanic or Latino
   </label>
   <Modal
     idName="AppEthFreeModal"
@@ -14,9 +21,23 @@
     modalBody="<p> Specify in text the Applicant or borrower's Other Hispanic
     or Latino ethnicity(ies) provided by the Applicant or Borrower. </p>
     <p>Otherwise, leave this data field blank</p>" />
-  <input
-    class="form-control"
-    type="text"
-    id="EthFree"
-    bind:value={$applicantEthOther}
-    on:change={applicantEthOther.change} />
+  {#if $applicantEth1 == '14' || $applicantEth2 == '14' || $applicantEth3 == '14' || $applicantEth4 == '14' || $applicantEth5 == '14'}
+    <input
+      class="form-control"
+      type="text"
+      id="EthFree"
+      bind:value={$applicantEthOther}
+      on:change={applicantEthOther.change} />
+    {#if $applicantEthOther !== ""}
+      <HRValidation isValid={true} />
+    {:else}
+      <HRValidation/>
+    {/if}
+  {:else}
+      <input
+      class="form-control"
+      type="text"
+      id="EthFree"
+      bind:value={$applicantEthOther}
+      on:change={applicantEthOther.change} />
+  {/if}
--- src/components/CoApplicant GMI/EthFree.svelte
+++ src/components/CoApplicant GMI/EthFree.svelte
@@ -1,12 +1,18 @@
 <script>
     import {
         coapplicantEthOther,
+        coapplicantEth1,
+        coapplicantEth2,
+        coapplicantEth3,
+        coapplicantEth4,
+        coapplicantEth5,
         NoCoApp
     } from "./../../stores.js";
     import Modal from "../Modal.svelte";
+    import HRValidation from "../HRValidation.svelte";
 </script>
 
-<label for="CoEthFree" style="font-size: 8px;">
+<label for="CoEthFree" style="font-size: 12px;">
 Free Form for Other Hispanic or Latino
 </label>
 <Modal
@@ -15,10 +21,25 @@ modalTitle="Co-Applicant Ethnicity: Free Form for Other Hispanic or Latino"
 modalBody="<p> Specify in text the Co-Applicant or Co-Borrower's Other
 Hispanic or Latino ethnicity(ies) provided by the Co-Applicant or
 Co-Borrower. </p> <p>Otherwise, leave this data field blank</p>" />
-<input
-class="form-control"
-type="text"
-id="CoEthFree"
-bind:value={$coapplicantEthOther}
-on:change={coapplicantEthOther.change}
-disabled={$NoCoApp} />
+{#if $coapplicantEth1 == '14' || $coapplicantEth2 == '14' || $coapplicantEth3 == '14' || $coapplicantEth4 == '14' || $coapplicantEth5 == '14'}
+    <input
+        class="form-control"
+        type="text"
+        id="CoEthFree"
+        bind:value={$coapplicantEthOther}
+        on:change={coapplicantEthOther.change}
+        disabled={$NoCoApp} />
+    {#if $coapplicantEthOther !== ""}
+        <HRValidation isValid={true} />
+    {:else}
+        <HRValidation/>
+    {/if}
+{:else}
+    <input
+    class="form-control"
+    type="text"
+    id="CoEthFree"
+    bind:value={$coapplicantEthOther}
+    on:change={coapplicantEthOther.change}
+    disabled={$NoCoApp} />
+{/if}
