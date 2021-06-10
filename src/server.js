9e5c4fb8521212a2ff48ba30c34ed439100fc4f6
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-06-10 09:10:59 -0400

    Switch to HTTPS
    Issues working locally from serving over HTTP vs HTTPS. Push to Netlify to confirm it server config doesn't break that portion.

--- src/server.js
+++ src/server.js
@@ -2,20 +2,31 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+const devCerts = require('office-addin-dev-certs');
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-  .use(
-    compression({
-      threshold: 0,
-    }),
-    sirv('static', {
-      dev,
-    }),
-    sapper.middleware()
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log('error', err);
-  });
+const { createServer } = require('https');
+const ssl_port = 443;
+
+initServer();
+
+async function initServer() {
+	const devCertOptions = await devCerts.getHttpsServerOptions();
+
+	let options = {
+		key: devCertOptions.key,
+		cert: devCertOptions.cert,
+	};
+
+	const { handler } = polka().use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	);
+
+	createServer(options, handler).listen(ssl_port, (_) => {
+		console.log(`> Running on https://localhost:${ssl_port}`);
+	});
+}
