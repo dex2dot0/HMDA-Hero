52db497c007e8b8597ecb251748612440f414c48
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-06-11 08:59:40 -0400

    Formatting

--- src/components/Loan Details/ActionTakenDate.svelte
+++ src/components/Loan Details/ActionTakenDate.svelte
@@ -1,12 +1,14 @@
 <script>
-  import { actionTakenDate } from './../../stores.js';
+  import {
+      actionTakenDate
+  } from "./../../stores.js";
 
-  import HRValidation from '../HRValidation.svelte';
-  import checkValidDate from '../../Scripts/checkValidDate';
-  import DataFieldFormat from '../DataFieldFormat.svelte';
-  import RegLink from '../RegLink.svelte';
-  import QualityErrors from '../QualityErrors.svelte';
-  import ValidityErrors from '../ValidityErrors.svelte';
+  import HRValidation from "../HRValidation.svelte";
+  import checkValidDate from "../../Scripts/checkValidDate";
+  import DataFieldFormat from "../DataFieldFormat.svelte";
+  import RegLink from "../RegLink.svelte";
+  import QualityErrors from "../QualityErrors.svelte";
+  import ValidityErrors from "../ValidityErrors.svelte";
 </script>
 
 <DataFieldFormat>
@@ -14,16 +16,9 @@
     <label for="ActionDate">Action Date</label>
   </span>
   <span slot="helpers">
-    <RegLink
-      regUrl="https://www.consumerfinance.gov/rules-policy/regulations/1003/4/#a-8-ii" />
-    <QualityErrors
-      errors="1"
-      idName="actionTakenDateQualityErrors"
-      errorsHTML="" />
-    <ValidityErrors
-      errors="2"
-      idName="actionTakenDateValidityErrors"
-      errorsHTML="" />
+    <RegLink regUrl="https://www.consumerfinance.gov/rules-policy/regulations/1003/4/#a-8-ii"/>
+    <QualityErrors errors=1 idName="actionTakenDateQualityErrors" errorsHTML=""/>
+    <ValidityErrors errors=2 idName="actionTakenDateValidityErrors" errorsHTML=""/>
   </span>
   <span slot="input">
     <input
@@ -31,13 +26,13 @@
       type="date"
       id="ActionDate"
       tabindex="0"
-      bind:value="{$actionTakenDate}"
-      on:change="{actionTakenDate.change}" />
+      bind:value={$actionTakenDate}
+      on:change={actionTakenDate.change} />
     {#await checkValidDate($actionTakenDate) then isFormattedDate}
       {#if $actionTakenDate !== '' || isFormattedDate}
-        <HRValidation isValid="{true}" />
+        <HRValidation isValid={true} />
       {:else}
-        <HRValidation isValid="{false}" />
+        <HRValidation isValid={false} />
       {/if}
     {/await}
   </span>
