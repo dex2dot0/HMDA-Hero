db3f474ba17067807207a2936af53e9a19b51c8f
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-06-11 20:42:17 -0400

    Formatting

--- src/Excel Scripts/getEndRow.js
+++ src/Excel Scripts/getEndRow.js
@@ -1,32 +1,36 @@
 export async function getEndRow() {
-  return new Promise((resolve, reject) => {
-    try {
-      Excel.run(async (context) => {
-        let sheet = context.workbook.worksheets.getItem('Data');
-        let usedRange = sheet.getRange('B:B');
-        let valuesRange = usedRange.getSpecialCells(
-          Excel.SpecialCellType.constants,
-          Excel.SpecialCellValueType.numbersText
-        );
-        //ex: B2:D7
-        valuesRange.load('address');
+	return new Promise((resolve, reject) => {
+		try {
+			Excel.run(async (context) => {
+				let sheet = context.workbook.worksheets.getItem('Data');
+				let usedRange = sheet.getRange('B:B');
+				let valuesRange = usedRange.getSpecialCells(
+					Excel.SpecialCellType.constants,
+					Excel.SpecialCellValueType.numbersText
+				);
+				//ex: B2:D7
+				valuesRange.load('address');
 
-        context.sync().then(async function () {
-          //Need to get the ending row
-          let vRange = valuesRange.address;
-          let endRow = vRange.slice(9, vRange.length);
-          console.log(`end row is ${endRow}`);
-          resolve(endRow);
-        });
-      });
-    } catch (error) {
-      console.log(error);
-      if (error.message.includes('Excel is not defined')) {
-        //return default data for Example LAR file
-        resolve('7');
-      } else {
-        reject(error);
-      }
-    }
-  });
+				context.sync().then(async function () {
+					//Need to get the ending row
+					let vRange = valuesRange.address;
+					let vRangeArr = vRange.split(',');
+					let endingRangeVal;
+					vRangeArr.length > 0 ? (endingRangeVal = vRangeArr[vRangeArr.length]) : vRange;
+					console.log(endingRangeVal);
+					let endRow = vRange.slice(9, vRange.length);
+					console.log(`end row is ${endRow}`);
+					resolve(endRow);
+				});
+			});
+		} catch (error) {
+			console.log(error);
+			if (error.message.includes('Excel is not defined')) {
+				//return default data for Example LAR file
+				resolve('7');
+			} else {
+				reject(error);
+			}
+		}
+	});
 }
