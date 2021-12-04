aec521dda140eae1234259157d8cb7e67c85fec2
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2021-12-04 00:46:00 -0500

    Better message for https URL

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-require('dotenv').config()
+require('dotenv').config();
 const { readFileSync } = require('fs');
 
 const { PORT, NODE_ENV } = process.env;
@@ -24,24 +24,26 @@ function runProductionServer() {
 async function runLocalServer() {
 	try {
 		if (!process.env.KEYPATH || !process.env.CERTPATH) {
-			console.error('You must install certs prior to running a local build. Run "npm run certs" to install locally. See the README for more info.');
+			console.error(
+				'You must install certs prior to running a local build. Run "npm run certs" to install locally. See the README for more info.'
+			);
 			throw 'Missing local certs';
 		}
 		let options = {
 			key: readFileSync(process.env.KEYPATH),
 			cert: readFileSync(process.env.CERTPATH),
 		};
-	
+
 		const { handler } = polka().use(
 			compression({ threshold: 0 }),
 			sirv('static', { dev }),
 			sapper.middleware()
 		);
-	
+
 		createServer(options, handler).listen(ssl_port, (_) => {
-			console.log(`> Running on https://localhost:${ssl_port}. Server should not listen on port 3000, ignore error`);
+			console.log(
+				`> Running on https://localhost:${ssl_port}. You must explicitly open to https://localhost:443. Server should not listen on port 3000, ignore error`
+			);
 		});
-	} catch (error) {
-		
-	}
+	} catch (error) {}
 }
